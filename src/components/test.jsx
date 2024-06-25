bfac65e7eafb078c5cb5b93cf43a135ed37a46df
Aaroodh <aaroodhl94@gmail.com>
2024-06-25 23:25:13 +0530

    test/pr

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Test = ()=>{
     return <>
-    <p> this is the testing component for the react app that i have been developing</p>
+        <p> this is the testing component for the react app that i have been developing</p>
+        <p> I have added this extra line to test the git pr</p>
     </>
 }
